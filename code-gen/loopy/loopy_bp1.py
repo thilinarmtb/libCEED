e1de81ab738466452868bb64dd6b05bde36bfa1f
Christensen <njchris2@illinois.edu>
2019-02-25 20:04:38 -0600

    function improvements

--- code-gen/loopy/loopy_bp1.py
+++ code-gen/loopy/loopy_bp1.py
@@ -15,29 +15,30 @@ loopy.options.ALLOW_TERMINAL_COLORS = False
 #ctx = lp.ArrayArg("ctx", dtype=np.int32, address_space=lp.auto)
 
 masssetupf = lp.make_kernel(
-    "{ [i, d,dd]: 0<=i<Q and 0<=d,dd<3 }",
+    "{ [i, d]: 0<=i<Q and 0<=d<3 }",
     """
     <> dummy = ctx[0] # Need to figure out how to remove
     for i
     <> offset = iOf7[1] + i
-    for dd
-        <> e = (dd + 1) % 3 
-        <> f = (dd + 2) % 3
-        <> index0 = offset + (0 + dd)*Q 
+
+    for d
+        <> e = (d + 1) % 3 
+        <> f = (d + 2) % 3
+        <> index0 = offset + (0 + d)*Q 
         <> index1 = offset + (3 + e)*Q
         <> index2 = offset + (6 + f)*Q
         <> index3 = offset + (6 + e)*Q
         <> index4 = offset + (3 + f)*Q
-        
-        <> detv = (-1**dd)*in[index0] * \
+
+        <> val = in[d*Q + i + iOf7[0]]**2 
+        <> detv = (-1**d)*in[index0] * \
             (in[index1]*in[index2] - in[index3]*in[index4])
     end
-
-    <> det = sum(dd, detv)
+    <> w = sqrt(simul_reduce(sum, d, val))
+    <> det = simul_reduce(sum, d, detv)
     <> val1 = det*in[i + iOf7[2]]
     out[i + oOf7[0]] = val1
-    out[i + oOf7[1]] = val1 * sqrt(sum(d, in[d*Q + i + iOf7[0] ]))
-
+    out[i + oOf7[1]] = val1 * w 
     end
     """,
     name="masssetupf",
