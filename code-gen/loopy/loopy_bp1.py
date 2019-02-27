85d1131a352d3d23f3659bcf0b1bbd26e08d74e3
Nicholas Christensen <njchris2@illinois.edu>
2019-02-27 02:29:50 -0600

    push some optimizations

--- code-gen/loopy/loopy_bp1.py
+++ code-gen/loopy/loopy_bp1.py
@@ -15,50 +15,59 @@ loopy.options.ALLOW_TERMINAL_COLORS = False
 #ctx = lp.ArrayArg("ctx", dtype=np.int32, address_space=lp.auto)
 
 masssetupf = lp.make_kernel(
-    "{ [i, d]: 0<=i<Q and 0<=d<3 }",
+    "{ [i,d,dd]: 0<=i<Q and 0<=d,dd<3 }",
     """
-    <> dummy = ctx[0] # Need to figure out how to remove
-    for i
-        <> offset = iOf7[1] + i
-        for d
-            <> e = (d + 1) % 3 
-            <> f = (d + 2) % 3
-            <> index0 = offset + (0 + d)*Q 
-            <> index1 = offset + (3 + e)*Q
-            <> index2 = offset + (6 + f)*Q
-            <> index3 = offset + (6 + e)*Q
-            <> index4 = offset + (3 + f)*Q
+    val := in[dd*Q + iOf7[0]]**2
+    w := sqrt(simul_reduce(sum, dd, val))
+    e := (d + 1) % 3
+    f := (d + 2) % 3
+    os0 := iOf7[1] + (0 + d)*Q 
+    os1 := iOf7[1] + (3 + e)*Q
+    os2 := iOf7[1] + (6 + f)*Q
+    os3 := iOf7[1] + (6 + e)*Q
+    os4 := iOf7[1] + (3 + f)*Q
+    s := -1**d
 
-            <> val = in[d*Q + i + iOf7[0]]**2 
-            <> detv = (-1**d)*in[index0] * \
-                (in[index1]*in[index2] - in[index3]*in[index4])
-        end
-        <> w = sqrt(simul_reduce(sum, d, val))
-        <> det = simul_reduce(sum, d, detv)
-        <> val1 = det*in[i + iOf7[2]]
-        out[i + oOf7[0]] = val1
-        out[i + oOf7[1]] = val1 * w 
-    end
+    <> dummy = ctx[0] # Need to figure out how to remove
+
+    <> det = sum(d, s*in[i+os0] * (in[i+os1]*in[i+os2] - in[i+os3]*in[i+os4]))
+    <> val1 = det*in[i + iOf7[2]]
+    out[i + oOf7[0]] = val1
+    out[i + oOf7[1]] = val1 * w 
     """,
     name="masssetupf",
-    assumptions="Q >= 0",
+    assumptions="Q > 0",
     kernel_data=["ctx", "Q", "iOf7", "oOf7", "in", "out"],
     target=lp.OpenCLTarget()
     )
+for os in ["e","f","os0","os1","os2","os3","os4","s"]:
+    masssetupf = lp.precompute(masssetupf, os, "d")
+masssetupf = lp.precompute(masssetupf, "w")
+masssetupf = lp.prioritize_loops(masssetupf,"i,d")
+masssetupf = lp.tag_inames(masssetupf, {"d": "unr"})
+#masssetupf = lp.split_iname(masssetupf, "i", 8, inner_tag="ilp", slabs=(0,1))
+
 #print(setup)    
 
 massf = lp.make_kernel(
     "{ [i]: 0<=i<Q }",
     """
+    o_os := i + oOf7[0]
+    i_os0 := i + iOf7[0]
+    i_os1 := i + iOf7[1]
     <> dummy = ctx[0]
-    out[i + oOf7[0]] = in[i + iOf7[0]] * in[i + iOf7[1]]
+    out[o_os] = in[i_os0] + in[i_os1]
+    #out[i+oOf7[0]] = in[i+iOf7[0]] * in[i+iOf7[1]]
     """,
     name="massf",
-    assumptions="Q >= 0",
+    assumptions="Q > 0",
     kernel_data=["ctx", "Q", "iOf7", "oOf7", "in", "out"],
     target=lp.OpenCLTarget()
     )
-#print(mass)
+print(massf)
+#massf = lp.split_iname(massf, "i", 8, outer_tag="g.0", inner_tag="ilp", slabs=(0,1))
+#massf = lp.split_array_axis(massf, "out,in", axis_nr=0, count=4)
+#massf = lp.tag_array_axes(massf, "out, in", "C,vec")
 
 kernelList1 = [masssetupf, massf]
 
