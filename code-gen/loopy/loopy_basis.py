4c373ae82beb5fbfd4129bda3e1908289a48420a
Nicholas Christensen <njchris2@illinois.edu>
2019-03-16 18:17:08 -0500

    push latest version

--- code-gen/loopy/loopy_basis.py
+++ code-gen/loopy/loopy_basis.py
@@ -13,79 +13,76 @@ filterwarnings('error', category=lp.LoopyWarning)
 import loopy.options
 loopy.options.ALLOW_TERMINAL_COLORS = False
 
-ctx = cl.create_some_context(interactive=False)
-queue = cl.CommandQueue(ctx)
-
-'''
-x_vec_dev = cl.clrandom.rand(queue, n, dtype=np.float32)
-y_vec_dev = cl.clrandom.rand(queue, n, dtype=np.float32)
-z_vec_dev = cl.clrandom.rand(queue, n, dtype=np.float32)
-a_mat_dev = cl.clrandom.rand(queue, (n, n), dtype=np.float32)
-b_mat_dev = cl.clrandom.rand(queue, (n, n), dtype=np.float32)
-x_mat_dev = cl.array.Array(queue, (n, n), dtype=np.float32)
-x_mat_host = np.float32(np.random.rand(n,n))
-a_mat_host = np.float32(np.random.rand(n,n))
-x_vec_host = np.random.randn(n).astype(np.float32)
-y_vec_host = np.random.randn(n).astype(np.float32)
-'''
-
 # ------
-
-kZero = lp.make_kernel(
-    "{ [e,i]: 0<=e<nelem and 0<=i<vsize }",
-    """
-    m := nc*elemsize
-    v[e*m + i] = 0
-    """,
-    name="kZero",
-    assumptions="nelem > 0 and vsize > 0",
-    target=lp.OpenCLTarget()
-)
-
-kZero = lp.prioritize_loops(kZero, "e,i")
-kZero = lp.precompute(kZero, "m")
-kZero = lp.tag_inames(kZero, {"e":"g.0"}) 
-### Not sure why this is not working
-#kZero = lp.split_iname(kZero, "i", 4, inner_tag="vec")
-#kRestrict6 = lp.split_array_axis(kZero, "v", axis_nr=0, count=4)
-#kRestrict6 = lp.tag_array_axes(kZero, "v", "C,vec")
-# l.0 same length as cache line
-kZero = lp.split_iname(kZero, "i", 8, inner_tag="ilp",outer_tag="g.1", slabs=(0,1))
-
-kCeedTensorContract = lp.make_kernel(
-    ["{ [a,b,c,j]: 0<=a<A and 0<=b<B and 0<=c<C and 0<=j<J }"
-     #"{ [c_wxs]: wxs_os<=c_wxs<C+wxs_os}",
-     #"{ [c_rxs]: rxs_os<=c_rxs<C+rxs_os}"
-    ],
-    """
-    <> tstride0 = if(transpose, 1, B)
-    <> tstride1 = if(transpose, J, 1)
-    for a,j,c
-        <> wxs = ((a*J+j)*C+c) + wxs_os
-        <> rxs = ((a*B+b)*C+c) + rxs_os
-        #v[wxs] = if(Add, v[wxs], 0) + t[j*stride0 + b*stride1] * u[rxs]
-        #All choose the same so this flow control should be fine
-        if Add 
-            v[wxs] = v[wxs] + t[j*stride0 + b*stride1] * u[rxs]
-        else
-            v[wxs] = t[j*stride0 + b*stride1] * u[rxs]
-        end 
-    end
-    #Better for GPU?
-    #v[a,j,c_wxs] = if(Add,v[a,j,c_wxs],0) + t[j*stride0 + b*stride1] * u[a,b,c_rxs]
-  
-    #Better for CPU?
-    #if Add 
-    #    v[a,j,c_wxs] = v[a,j,c_wxs] + t[j*stride0 + b*stride1] * u[a,b,c_rxs]
-    #else
-    #    v[a,j,c_wxs] = t[j*stride0 + b*stride1] * u[a,b,c_rxs]
-    #end
-
-    """,
-    name = "kCeedTensorContract",
-    target=lp.OpenCLTarget(),
-    assumptions="A>0 and B>0 and C>0 and J>0"
+def generate_kZero(arch="INTEL_CPU", fp_format=np.float64, target=lp.OpenCLTarget()):
+    kZero = lp.make_kernel(
+        "{ [e,i]: 0<=e<nelem and 0<=i<vsize }",
+        """
+        m := nc*elemsize
+        v[e*m + i] = 0
+        """,
+        name="kZero",
+        assumptions="nelem > 0 and vsize > 0",
+        target=target
     )
+    kZero = lp.add_and_infer_dtypes(kZero, {"v": fp_format, "elemsize": np.int32, "nc": np.int32})
+
+    kZero = lp.tag_inames(kZero, {"e":"g.1"}) 
+    if arch == "AMD_GPU":
+        kZero = lp.split_iname(kZero, "i", 64, inner_tag="l.0",outer_tag="g.0", slabs=(0,1))
+    elif arch == "NVIDIA_GPU":
+        kZero = lp.split_iname(kZero, "i", 32, inner_tag="l.0",outer_tag="g.0", slabs=(0,1))
+    else:
+        kZero = lp.split_iname(kZero, "i", 128, inner_tag="l.0",outer_tag="g.0", slabs=(0,1))
+
+    return kZero
+
+def generate_kCeedTensorContract(arch="INTEL_CPU", fp_format=np.float64, target=lp.OpenCLTarget()):
+    kCeedTensorContract = lp.make_kernel(
+        ["{ [a,b,c,j]: 0<=a<A and 0<=b<B and 0<=c<C and 0<=j<J }"],
+        """
+        wxs := ((a*J+j)*C+c) + wxs_os
+        rxs := ((a*B+b)*C+c) + rxs_os
+         
+        if Add
+            if transpose
+                v[wxs] = v[wxs] + t[j + b*J] * u[rxs] {id=Add_t}
+            else
+                v[wxs] = v[wxs] + t[j*B + b] * u[rxs] {id=Add}
+            end
+        else
+            if transpose
+                v[wxs] = t[j + b*J] * u[rxs] {id=no_Add_t}
+            else 
+                v[wxs] = t[j*B + b] * u[rxs] {id=no_Add}
+            end
+        end 
+        """,
+        name = "kCeedTensorContract",
+        target=target,
+        assumptions="A>0 and B>0 and C>0 and J>0"
+        )
+
+    kCeedTensorContract = lp.prioritize_loops(kCeedTensorContract, "a,j,b,c")
+    kCeedTensorContract = lp.duplicate_inames(kCeedTensorContract,"a,b,c,j", within="id:no_Add")
+    kCeedTensorContract = lp.duplicate_inames(kCeedTensorContract,"a,b,c,j", within="id:no_Add_t")
+    kCeedTensorContract = lp.duplicate_inames(kCeedTensorContract,"a,b,c,j", within="id:Add")
+
+    # Figure out some additional optimizations here 
+
+    kCeedTensorContract = lp.add_and_infer_dtypes(kCeedTensorContract, {
+        "v": fp_format, 
+        "u": fp_format, 
+        "t": fp_format, 
+        "transpose": np.byte, 
+        "Add": np.byte,
+        "wxs_os": np.int32,
+        "rxs_os": np.int32
+        }
+    )
+    return kCeedTensorContract
+ 
+
 #print(kCeedTensorContract)
 
 
@@ -164,6 +161,8 @@ kInterp = lp.make_kernel(
     name="kInterp",
     target=lp.OpenCLTarget(),
     assumptions="nelem>0 and dim>0 and pre>0 and post>0 and P>0 and Q>0")
+
+
 #print(kInterp)
 
 #Surely there is a way to combine kGrad and kInterp. They are mostly identical codewise
@@ -288,32 +287,35 @@ kWeight = lp.make_kernel(
     v_shift := (QnD*nc + QnD*nc*dim)
     m := QnD*nc*(dim + 2) 
     v_offset := e*m + v_shift
+    QQ := Q*Q
     ii := Q*i + k
     xs0 := ((ii * Q + j)    + 0 ) + v_offset
-    xs1 := ((i  * Q + j)*Q  + k ) + v_offset
-    xs2 := ((0  * Q + j)*QQ + ii) + v_offset
-    QQ := Q**2
+    #xs1 := ((i  * Q + j)*Q  + k ) + v_offset
+    xs1 := i*QQ + j*Q + k + v_offset
+    xs2 := ((0  * Q + j)*Q*Q + ii) + v_offset
+    qw(s) := qweight1d[s]
     #xs := ((i*Q**(dim-d-1) + j)*Q + k) + v_offset
-
+    
     #post := Q**d
     #pre := Q**(dim-d-1)
     #xs := ((i*Q + j)*post + k) + v_offset
     #<> val = pre #For some reason won't generate val without this
-
-    for e, i,j,k
+    for e,i,j,k
+        <> ind = xs1
+        d_v[ind] = qw(i)*qw(j)*qw(k)*d_v[ind]**2
         # For contiguous accesses need separate loops with separate orderings?
         #Race condition if do this? 
         #Ideal way to do this is probably coordinate so all values
         # local memory.
-        d_v[xs0] = qweight1d[j] {id=d0} #Possibly tag each for separate loops #i,k,j
-        with {id_prefix=d1}
-            <> ind1 = xs1
-            d_v[ind1] = qweight1d[j]*d_v[ind1] #i,j,k
-        end
-        with {id_prefix=d2}
-            <> ind2 = xs2
-            d_v[ind2] = qweight1d[j]*d_v[ind2]#j,i,k
-        end
+        #d_v[xs0] = qweight1d[j] {id=d0} #Possibly tag each for separate loops #i,k,j
+        #with {id_prefix=d1}
+        #    <> ind1 = xs1
+        #    d_v[ind1] = qweight1d[j]*d_v[ind1] #i,j,k
+        #end
+        #with {id_prefix=d2}
+        #    <> ind2 = xs2
+        #    d_v[ind2] = qweight1d[j]*d_v[ind2]#j,i,k
+        #end
         #Above xs is equivalent to below?
         #<> post = Q**d
         #<> pre = Q**(dim-d-1) 
@@ -342,54 +344,49 @@ kWeight = lp.make_kernel(
     target=lp.OpenCLTarget(),
     assumptions="nelem>0 and dim>0 and Q>0"
     )
+
 kWeight = lp.precompute(kWeight, "v_shift")
 kWeight = lp.precompute(kWeight, "m")
-kWeight = lp.add_prefetch(kWeight, "qweight1d","j")
 kWeight = lp.precompute(kWeight, "QQ")
+kWeight = lp.prioritize_loops(kWeight,"e,i,j,k")
+kWeight = lp.add_prefetch(kWeight, "qweight1d", "i,j,k")
 kWeight = lp.precompute(kWeight, "v_offset")
-kWeight = lp.duplicate_inames(kWeight,"i,j,k", within="id:d0")
-kWeight = lp.duplicate_inames(kWeight,"i,j,k", within="id:d1*")
-kWeight = lp.duplicate_inames(kWeight,"i,j,k", within="id:d2*")
-kWeight = lp.prioritize_loops(kWeight, "i_0,k_0,j_0")
-kWeight = lp.prioritize_loops(kWeight, "i_1,j_1,k_1")
-kWeight = lp.prioritize_loops(kWeight, "j_2,i_2,k_2")
+#kWeight = lp.duplicate_inames(kWeight,"i,j,k", within="id:d0")
+#kWeight = lp.duplicate_inames(kWeight,"i,j,k", within="id:d1*")
+#kWeight = lp.duplicate_inames(kWeight,"i,j,k", within="id:d2*")
+#kWeight = lp.prioritize_loops(kWeight, "i_0,k_0,j_0")
+#kWeight = lp.prioritize_loops(kWeight, "i_1,j_1,k_1")
+#kWeight = lp.prioritize_loops(kWeight, "j_2,i_2,k_2")
 #kWeight = lp.tag_inames(kWeight, [("e", "g.0")])
 #kWeight = lp.precompute(kWeight, "xs2(ii,j)")
 #kWeight = lp.tag_inames(kWeight, [("d", "unr")])
 #kWeight = lp.precompute(kWeight, "post", "d")
 #kWeight = lp.precompute(kWeight, "pre", "d")
-#kWeight = lp.prioritize_loops(kWeight,"e,d,i,j,k")
 #print(kWeight)
 
+kZero = generate_kZero()
 kernelList1 = [kZero]
+kCeedTensorContract = generate_kCeedTensorContract()
 kernelList2 = [kCeedTensorContract]
 kernelList3 = [kInterp]
 kernelList4 = [kGrad]
 kernelList5 = [kWeight]
 
+'''
 for k in kernelList1:
-    k = lp.set_options(k, "write_cl")
-    k = lp.add_and_infer_dtypes(k, {"v": np.float64, "elemsize": np.int32, "nc": np.int32})
+    print(k)
+    print()
     code = lp.generate_code_v2(k).device_code()
     print(code)
+'''
 
 for k in kernelList2:
-    k = lp.set_options(k, "write_cl")
-    k = lp.add_and_infer_dtypes(k, {
-        "v": np.float64, 
-        "u": np.float64, 
-        "t": np.float64, 
-        "transpose": np.byte, 
-        "Add": np.byte,
-        "stride0": np.int32,
-        "stride1": np.int32,
-        "wxs_os": np.int32,
-        "rxs_os": np.int32
-        }
-    )
+    print(k)
+    print()
     code = lp.generate_code_v2(k).device_code()
     print(code)
 
+'''
 for k in kernelList3:
     k = lp.set_options(k, "write_cl")
     k = lp.add_and_infer_dtypes(k, {
@@ -449,3 +446,4 @@ for k in kernelList5:
     })
     code = lp.generate_code_v2(k).device_code()
     print(code)
+'''
