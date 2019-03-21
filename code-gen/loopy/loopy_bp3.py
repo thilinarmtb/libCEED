fea280046d9a92b7fe9f9a7aeac1624c51e38856
Nicholas Christensen <njchris2@illinois.edu>
2019-03-21 18:47:03 -0500

    update bp3 kernel

--- code-gen/loopy/loopy_bp3.py
+++ code-gen/loopy/loopy_bp3.py
@@ -12,106 +12,108 @@ filterwarnings('error', category=lp.LoopyWarning)
 import loopy.options
 loopy.options.ALLOW_TERMINAL_COLORS = False
 
-#ctx = cl.create_some_context(interactive=True)
-#queue = cl.CommandQueue(ctx)
+def generate_diffsetupf(constants={}, arch="INTEL_CPU", fp_format=np.float64, target=lp.OpenCLTarget()):
 
-#x_vec_dev = cl.clrandom.rand(queue, n, dtype=np.float32)
-#y_vec_dev = cl.clrandom.rand(queue, n, dtype=np.float32)
-#z_vec_dev = cl.clrandom.rand(queue, n, dtype=np.float32)
-#a_mat_dev = cl.clrandom.rand(queue, (n, n), dtype=np.float32)
-#b_mat_dev = cl.clrandom.rand(queue, (n, n), dtype=np.float32)
-#x_mat_dev = cl.array.Array(queue, (n, n), dtype=np.float32)
-#x_mat_host = np.float32(np.random.rand(n,n))
-#a_mat_host = np.float32(np.random.rand(n,n))
-#x_vec_host = np.random.randn(n).astype(np.float32)
-#y_vec_host = np.random.randn(n).astype(np.float32)
-# create
-# ------
+    diffsetupf = lp.make_kernel(
+        ["{ [i,j,k]: 0<=i<Q and 0<=j,k<3}",
+         "{ [kkk,jjj,lll]: 0<=kkk,lll,jjj<3}" ],
+        """
+        # For reference
+        #    *x = in + iOf7[0],
+        #    *J = in + iOf7[1];
+        #    *w = in + iOf7[2];
+        #    *rho = out + oOf7[0]
+        #    *rhs = out + oOf7[1]
 
-setupf = lp.make_kernel(
-    ["{ [i,j,k]: 0<=i<Q and 0<=j,k<3}",
-     "{ [kk,jj]: 0<=jj,kk<3}",
-     "{ [kkk,jjj,lll]: 0<=kkk,lll,jjj<3}" ],
-    """
-    # For reference
-    #    *J = in + iOf7[0],
-    #    *qw = in + iOf7[1];
-    #    *qd = out + oOf7[0];
+        M_PI := 3.14159265358979323846
+        iind1 := i + iOf7[1]
+        oind1 := i + oOf7[1]
+        iind2 := i + iOf7[2]
 
-    for i
-        <> iind0 = i + iOf7_0
-        <> oind0 = i + oOf7_0
-        for jj,kk
-            <> J[jj,kk] = in[3*Q*jj + Q*kk + iind0]
+    
+        J(jj,kk) := in[3*Q*jj + Q*kk + iind1]
+
+        for i
+            for j,k
+
+                <> m = (j + 1) % 3
+                <> n = (j + 2) % 3
+                <> o = (k + 1) % 3
+                <> p = (k + 2) % 3
+ 
+                <> A[k,j] = J(n,p)*J(m,o) - J(n,o)*J(m,p)
+            end
+            <> s =  sum(k, J(k,0)*A[0,k])
+            w := in[iind2] / s
+            for jjj,kkk
+                out[3*Q*jjj + Q*kkk + oind1] = w * sum(lll, A[jjj,lll]*A[kkk,lll])
+            end
+            rho := in[iind2] * s
+            out[oind1] = rho * M_PI**2 * (1**2 + 2**2 + 3**2) * \
+                            sin(M_PI*(0 + 1*in[iind1 + 0*Q])) * \
+                            sin(M_PI*(1 + 2*in[iind1 + 1*Q])) * \
+                            sin(M_PI*(2 + 3*in[iind1 + 2*Q])) 
+
+        end 
+        """,
+        name="diffsetupf",
+        assumptions="Q > 0",
+        target=target
+        )
+
+    diffsetupf = lp.fix_parameters(diffsetupf, **constants)
+    #diffsetupf = lp.tag_inames(diffsetupf, [("j", "unr"), ("k", "unr")])
+
+    diffsetupf = lp.add_and_infer_dtypes(diffsetupf, {
+        "in": fp_format,
+        "oOf7": np.int32,
+        "iOf7": np.int32,
+    })
+    
+
+    return diffsetupf
+
+def generate_diffusionf(constants={}, arch="INTEL_CPU", fp_format=np.float64, target=lp.OpenCLTarget()):
+    diffusionf = lp.make_kernel(
+        "{ [i,j]: 0<=i<Q }",
+        """
+        #    For Reference
+        #    *u = in  + iOf7[0],
+        #    *rho = in  + iOf7[1];
+        #    *v = out + oOf7[0];
+
+        ug(ii, jj) := in[iOf7[0] + ii + Q*jj]
+        rhog(ii,jj) := in[iOf7[1] + ii + Q*jj]
+
+        for i
+            out[oOf7[0] + i + Q*0] = rhog(i,0)*ug(i,0) + rhog(i,1)*ug(i,1) + rhog(i,2)*ug(i,2)
+            out[oOf7[1] + i + Q*1] = rhog(i,1)*ug(i,0) + rhog(i,3)*ug(i,1) + rhog(i,4)*ug(i,2)  
+            out[oOf7[1] + i + Q*2] = rhog(i,2)*ug(i,0) + rhog(i,4)*ug(i,1) + rhog(i,5)*ug(i,2)
         end
-        for j,k
-            <> m = (j + 1) % 3
-            <> n = (j + 2) % 3
-            <> o = (k + 1) % 3
-            <> p = (k + 2) % 3
-            <> A[k,j] = J[n,p]*J[m,o] - J[n,o]*J[m,p]
-        end
-        <> w = in[iOf7_1 + i] / sum(k, J[k,0]*A[0,k])
-        for jjj,kkk
-            out[3*Q*jjj + Q*kkk + oind0] = sum(lll, A[jjj,lll]*A[kkk,lll])
-        end
+        """,
+        name="diffusionf",
+        assumptions="Q > 0",
+        target=target
+        )
 
-        #for j,k,l
-        #    if k >= j
-        #        qd[j,k] = out[j,k,i+oOf7_0] + A[j,l]*A[k,l]
-        #    end
-        #end
-    end 
-    """,
-    name="setupf",
-    assumptions="Q >= 1",
-    target=lp.OpenCLTarget()
-    )
-print(setupf)    
+    diffusionf = lp.add_and_infer_dtypes(diffusionf, {
+        "in": fp_format,
+        "oOf7": np.int32,
+        "iOf7": np.int32,
+    })
+ 
+    return diffusionf
 
-diffusionf = lp.make_kernel(
-    "{ [i,j]: 0<=i<Q and 0<=j<3 }",
-    """
-    #    For Reference
-    #    *ug = in  + iOf7[0],
-    #    *qd = in  + iOf7[1];
-    #    *vg = out + oOf7[0];
+diffsetupf = generate_diffsetupf()
+code = lp.generate_code_v2(diffsetupf).device_code()
+print(diffsetupf)
+print()
+print(code)
+print()
 
-    for i, j
-        <> index = Q*j + i
-        out[index + oOf7_0] = in[index + iOf7_1] * in[index + iOf7_0]
-    end
-    """,
-    name="diffusionf",
-    assumptions="Q > 0",
-    target=lp.OpenCLTarget()
-    )
-print(diffusionf)
-
-kernelList1 = [setupf]
-kernelList2 = [diffusionf]
-
-for k in kernelList1:
-    k = lp.set_options(k, "write_cl")
-    k = lp.add_and_infer_dtypes(k, {
-        "in": np.float64,
-        "oOf7_0": np.int32,
-        "iOf7_0": np.int32,
-        "iOf7_1": np.int32
-        })
-    code = lp.generate_code_v2(k).device_code()
-    print(code)
-    print()
-
-for k in kernelList2:
-    k = lp.set_options(k, "write_cl")
-    k = lp.add_and_infer_dtypes(k, {
-        "in": np.float64,
-        "oOf7_0": np.int32,
-        "iOf7_0": np.int32,
-        "iOf7_1": np.int32
-        })
-    code = lp.generate_code_v2(k).device_code()
-    print(code)
-    print()
-
+diffusionf = generate_diffusionf()
+code = lp.generate_code_v2(diffusionf).device_code()
+print(diffsetupf)
+print()
+print(code)
+print()
