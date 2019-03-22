dc7541648d53ea9fe8c2082909cbd300f161baa6
Christensen <njchris2@illinois.edu>
2019-03-22 17:44:44 -0500

    undo changes to loop structure

--- code-gen/loopy/loopy_fused.py
+++ code-gen/loopy/loopy_fused.py
@@ -11,6 +11,27 @@ filterwarnings('error', category=lp.LoopyWarning)
 import loopy.options
 loopy.options.ALLOW_TERMINAL_COLORS = False
 
+import loopy_basis as basis
+import loopy_restrict as restrict
+
+
+r_base = restrict.generate_kRestrict6()
+
+kZero = basis.generate_kZero()
+r = lp.rename_argument(r_base, "vv", "v")
+print(r)
+restrict_kZero_fused = lp.fuse_kernels([r,kZero], data_flow=[])
+print(restrict_kZero_fused)
+
+'''
+kInterp = basis.generate_kInterp()
+r = lp.rename_argument(r_base, "vv", "d_v")
+restrict_kInterp_fused = lp.fuse_kernels([r, kInterp])
+print(restrict_kInterp_fused)
+'''
+
+
+'''
 def generate_fused_kernel():
     massf = lp.make_kernel("{ [iter]: 0<=iter<top }",
     """
@@ -22,5 +43,5 @@ def generate_fused_kernel():
 
 
 #def generate_fused_kernel(rk, bk, qk):
-    
+'''    
 
